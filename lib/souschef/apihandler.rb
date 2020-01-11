16d786e32f0bf35c5dc7318e8199e1d3b0cc1924
Wills Blake <richwblake@gmail.com>
2020-01-11 15:36:13 -0500

    Added search functionality to cli and apihandler. First letter/category

--- lib/souschef/apihandler.rb
+++ lib/souschef/apihandler.rb
@@ -1,13 +1,15 @@
+require 'pry'
+
 class Souschef::ApiHandler
-  @@BASE_URL = "https://www.themealdb.com/api/json/v1/1/search.php"
+  @@BASE_URL = "https://www.themealdb.com/api/json/v1/1/"
 
   def self.json_to_dish_args(json)
     data_hash = {}
-    json["meals"][0].each { |key, attribute|
-       puts data_hash[key] = attribute unless attribute == nil || attribute.strip == ""
-    }
 
-    # binding.pry
+    # cleans hash to include only key/value pairs where value is not nil or the empty string
+    json["meals"][0].each { |key, attribute|
+       data_hash[key] = attribute unless attribute == nil || attribute.strip == ""
+    }
 
     # creates array of ingredients of type String
     ingredients_array = []
@@ -21,8 +23,8 @@ class Souschef::ApiHandler
       measurements_array << attribute if key.include?("strMeasure")
     }
 
-
-    student_args = {
+    # creates a new hash to pass as an argument to Dish.new
+    dish_args = {
       "name" => data_hash["strMeal"],
       "category" => data_hash["strCategory"],
       "region" => data_hash["strArea"],
@@ -33,10 +35,30 @@ class Souschef::ApiHandler
   end
 
   def self.fetch_dish_by_name(name)
-    path = dish = @@BASE_URL + "?s=" + name
-    uri = URI(path)
+    response = self.generate_JSON_from_pathname("search.php?s=", name)
+    self.json_to_dish_args(response)
+  end
 
-    response = Net::HTTP.get(uri)
-    self.json_to_dish_args(JSON.parse(response))
+  def self.fetch_dishes_by_first_letter(letter)
+    self.generate_JSON_from_pathname("search.php?f=", letter)
+  end
+
+  def self.fetch_random_dish
+    response = self.generate_JSON_from_pathname("random.php")
+    self.json_to_dish_args(response)
+  end
+
+  def self.fetch_dish_categories
+    self.generate_JSON_from_pathname("categories.php")
+  end
+
+  def self.fetch_dishes_by_category(category)
+    self.generate_JSON_from_pathname("filter.php?c=", category)
+  end
+
+  def self.generate_JSON_from_pathname(extension, value = "")
+    path = @@BASE_URL + extension + value
+    uri = URI(path)
+    response = JSON.parse(Net::HTTP.get(uri))
   end
 end
--- lib/souschef/cli.rb
+++ lib/souschef/cli.rb
@@ -1,16 +1,48 @@
 class Souschef::CLI
   def call
     puts "Welcome to Souschef, let's get cookin'!"
-    find_dish_by_name
+    # find_dish_by_name
+    # list_dishes_by_first_letter
+    # find_random_dish
+    # list_dish_categories
+    # search_by_category
   end
 
   def get_input_from_user
-    input = gets.chomp
+    gets.chomp
   end
 
   def find_dish_by_name
     puts "Please enter the name of the dish:"
     dish = Souschef::Dish.new(Souschef::ApiHandler.fetch_dish_by_name(get_input_from_user))
+    print_dish(dish)
+  end
+
+  def list_dishes_by_first_letter
+    puts "Search dishes by which letter?:"
+    input = get_input_from_user
+    dishes = Souschef::ApiHandler.fetch_dishes_by_first_letter(input)
+    dishes["meals"].each { |dish| puts dish["strMeal"] }
+    find_dish_by_name
+  end
+
+  def list_dish_categories
+    puts "----------Categories----------"
+    categories = Souschef::ApiHandler.fetch_dish_categories
+    categories["categories"].each { |category| puts category["strCategory"]}
+  end
+
+  def search_by_category
+    puts "Please enter the category by which you'd like to search:"
+    input = get_input_from_user
+    dishes = Souschef::ApiHandler.fetch_dishes_by_category(input)
+    dishes["meals"].each{ |dish| puts dish["strMeal"] }
+    binding.pry
+  end
+
+  def find_random_dish
+    puts "Fetching you a random meal... How adventurous!"
+    dish = Souschef::Dish.new(Souschef::ApiHandler.fetch_random_dish)
     print_dish(dish)
   end
 
