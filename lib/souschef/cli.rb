8700044eb20ea3179ef6a8b4fc98c1bbf442a9e1
Wills Blake <richwblake@gmail.com>
2020-01-11 17:51:43 -0500

    Added category functionality. Added formatting to input.

--- lib/souschef/cli.rb
+++ lib/souschef/cli.rb
@@ -2,30 +2,60 @@ class Souschef::CLI
   def call
     puts "Welcome to Souschef 0.1.0, let's get cookin'!"
     menu
-    # find_dish_by_name
-    # list_dishes_by_first_letter
-    # find_random_dish
-    # list_dish_categories
-    # search_by_category
   end
 
+  # Prints menu format and options
   def menu
     puts "------------------"
     puts "       Menu       "
     puts "------------------"
-    puts "Welcome to the menu. You can choose a search method by simply\ntyping in the name of one of the methods below."
+    puts "Welcome to the menu. You can choose a search method by simply\ntyping in the name of one of the methods below. You can also exit\nby typing exit."
     puts ""
     puts " - Search by name"
     puts " - Search by first letter"
     puts " - Search by category"
     puts " - Get random dish"
-    print "\n:"
+    puts " - Exit"
+    puts ""
+
+    # Handles input and control flow for 1st level
+    # handle_menu_input returns the input as a String
+    menu_input = handle_menu_input
+
+    # Allows user to return to menu or exit program after a dish has been displayed
+    if menu_input != "exit"
+      puts "Press enter to return to the menu, or type anything to exit the application."
+      input = get_input_from_user
+      input == "" ? menu : exit
+    else
+      exit
+    end
+  end
+
+  def handle_menu_input
     input = get_input_from_user
 
+    case input
+    when "search by name"
+      find_dish_by_name
+    when "search by first letter"
+      list_dishes_by_first_letter
+    when "search by category"
+      list_dish_categories
+    when "get random dish"
+      find_random_dish
+    when "exit"
+      exit
+    else
+      print "Please try again\n"
+      handle_menu_input
+    end
+    input
   end
 
   def get_input_from_user
-    gets.chomp
+    print ":"
+    gets.chomp.strip.downcase
   end
 
   def find_dish_by_name
@@ -35,7 +65,7 @@ class Souschef::CLI
   end
 
   def list_dishes_by_first_letter
-    puts "Search dishes by which letter?:"
+    puts "Search dishes by which letter?"
     input = get_input_from_user
     dishes = Souschef::ApiHandler.fetch_dishes_by_first_letter(input)
     dishes["meals"].each { |dish| puts dish["strMeal"] }
@@ -46,6 +76,7 @@ class Souschef::CLI
     puts "----------Categories----------"
     categories = Souschef::ApiHandler.fetch_dish_categories
     categories["categories"].each { |category| puts category["strCategory"]}
+    search_by_category
   end
 
   def search_by_category
@@ -53,7 +84,7 @@ class Souschef::CLI
     input = get_input_from_user
     dishes = Souschef::ApiHandler.fetch_dishes_by_category(input)
     dishes["meals"].each{ |dish| puts dish["strMeal"] }
-    # binding.pry
+    find_dish_by_name
   end
 
   def find_random_dish
@@ -73,6 +104,11 @@ class Souschef::CLI
       puts "  #{index + 1}. #{dish.measurements[index]} #{ingredient}"
     }
     puts ""
-    puts "Instructions: \n#{dish.instructions.gsub(/\./, ".\n")}\n"
+    puts "Instructions: \n#{dish.instructions.gsub(/\./, ".\n")}"
+    puts "----------------------------------------------------------------------"
+  end
+
+  def exit
+    puts "See you in the kitchen soon!"
   end
 end
